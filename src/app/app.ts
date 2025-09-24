18e3b61cd4bbe7b9dcd5df23192743a59f4ad5fe
Ru-Rus <marquilleroruselljohn@gmail.com>
2025-09-24 09:55:19 +0800

    initial push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { headerComponent } from './header.components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ headerComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/header.components.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+
+@Component({
+selector: 'app-header',
+templateUrl: './header.component.html',
+standalone: true, //but as of now it is set to true in 20 up in 18 down false
+})
+
+export class headerComponent {};
